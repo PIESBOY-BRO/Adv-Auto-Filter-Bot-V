7ac035003cdb19e60a221f5c7c10c46e46766d6d
piesboy-bro <89738311+PIESBOY-BRO@users.noreply.github.com>
2021-10-05 00:26:23 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -64,25 +64,26 @@ async def auto_filter(bot, update):
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
             file_link = filter.get("file_link")
-            file_size = int(filter.get("file_size", "0"))
-            
-            # from B to MiB
+            file_size = int(filter.get("file_size", ""))
+            file_size = round((file_size/1024),2) # from B to KB
+            size = ""
+            file_KB = ""
+            file_MB = ""
+            file_GB = ""
             
             if file_size < 1024:
-                file_size = f"[{file_size} B]"
-            elif file_size < (1024**2):
-                file_size = f"[{str(round(file_size/1024, 2))} KiB] "
-            elif file_size < (1024**3):
-                file_size = f"[{str(round(file_size/(1024**2), 2))} MiB] "
-            elif file_size < (1024**4):
-                file_size = f"[{str(round(file_size/(1024**3), 2))} GiB] "
-            
-            
-            file_size = "" if file_size == ("[0 B]") else file_size
-            
-            # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
-            
+                file_KB = f"𝚂𝚞𝚋𝚝𝚒𝚝𝚕𝚎"
+                size = file_KB
+            elif file_size < (1024*1024):
+                file_MB = f"📂 {str(round((file_size/1024),2))} 𝙼ʙ"
+                size = file_MB
+            else:
+                file_GB = f"📂 {str(round((file_size/(1024*1024)),2))} 𝙶ʙ"
+                size = file_GB
+                
+            file_names = file_name
+            file_size = size
+            print(file_name)
 
             if file_type == "video":
                 if allow_video: 
